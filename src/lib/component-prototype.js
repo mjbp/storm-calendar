a29affa0757c0d899771cc01f39d2e323b03bf54
Mick Perkins <mickperkins@gmail.com>
2017-11-23 21:43:18 +0000

    Adds day incrementing experiments

--- src/lib/component-prototype.js
+++ src/lib/component-prototype.js
@@ -21,7 +21,30 @@ export default{
 		let totalDays = diffDays(this.startDate, this.endDate),
 			utilityDate = new Date(this.startDate.getTime());
 		
-		for (let i = 1; i <= totalDays; )
+		//for (let i = 1; i <= totalDays; )
+		//get date objects for each day between start and end
+		
+		
+		
+		
+		
+		//let numDays = Math.round(Math.abs((eventDates.startDate.getTime() - eventDates.endDate.getTime())/(24*60*60*1000)));
+		// let count = 0;
+		// let eventDateObjects = [];
+	
+		// let addDays = (date, days) => {
+		// 	let result = new Date(date);
+		// 	result.setDate(result.getDate() + days);
+		// 	return result;
+		// };
+	
+		// while(count <= numDays){
+		// 	eventDateObjects.push(addDays(eventDates.startDate, count));
+		// 	count++;
+		// }
+	
+	
+		console.log(eventDateObjects);
 
 
 		console.log(monthModel(this.startDate.getFullYear(), this.startDate.getMonth()));
